d882f30ea6c97af03363f900783d8e4405c7e51e
Piotr Stanny <piotr.stanny@gmail.com>
2020-04-23 12:13:55 +0100

    Attack method displaying alert and ending game once health of player or monster reached zero

--- app.js
+++ app.js
@@ -16,10 +16,23 @@ new Vue({
             var max = 10;
             var damage = Math.max(Math.floor(Math.random() * max + 1), min);
             this.monsterHealth -= damage;
+
+            if (this.monsterHealth <= 0) {
+                alert("You won!");
+                this.gameIsRunning = false;
+                return;
+            }
+
             min = 5;
             max = 12;
             damage = Math.max(Math.floor(Math.random() * max + 1), min);
             this.playerHealth -= damage;
+
+            if (this.playerHealth <= 0) {
+                alert("You lost!");
+                this.gameIsRunning = false;
+                return;
+            }
         },
         specialAttack: function (){
             
