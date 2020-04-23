d2d6eadcf5e813de62bfa455e2333e0fc9f55097
Piotr Stanny <piotr.stanny@gmail.com>
2020-04-23 12:05:22 +0100

    Attack method implemented

--- app.js
+++ app.js
@@ -10,6 +10,25 @@ new Vue({
             this.gameIsRunning = true,
             this.playerHealth = 100,
             this.monsterHealth = 100
+        },
+        attack: function(){
+            var min = 3;
+            var max = 10;
+            var damage = Math.max(Math.floor(Math.random() * max + 1), min);
+            this.monsterHealth -= damage;
+            min = 5;
+            max = 12;
+            damage = Math.max(Math.floor(Math.random() * max + 1), min);
+            this.playerHealth -= damage;
+        },
+        specialAttack: function (){
+            
+        },
+        heal: function(){
+
+        },
+        giveUp: function(){
+            this.gameIsRunning = false
         }
     }
 });
